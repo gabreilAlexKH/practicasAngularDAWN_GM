c8dbfacb0e101dc16d114a1bd28f7248a570a655
gabreilAlexKH <85039004+gabreilAlexKH@users.noreply.github.com>
2022-12-03 16:08:42 -0500

    Hibrida Rutas

--- hibrida/clienteAngular/src/app/app-routing.module.ts
+++ hibrida/clienteAngular/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FotoComponent } from './foto/foto.component';
+import { AlbumComponent } from './album/album.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: "foto", component: FotoComponent },
+  { path: "album", component: AlbumComponent },
+  { path: "**", redirectTo: "album" }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
