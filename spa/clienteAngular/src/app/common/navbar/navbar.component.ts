da019f37c4c29c77f80f5de68ec9723caca70563
Cliente CONTROL-PC <gabamald@espol.edu.ec>
2022-12-05 10:45:50 -0500

    inyeccion

--- spa/clienteAngular/src/app/common/navbar/navbar.component.ts
+++ spa/clienteAngular/src/app/common/navbar/navbar.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import {NavbarService} from 'src/app/servicios/navbar.service';
+import {Navbar , Nav} from 'src/app/interfaz/navbar';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
+
 export class NavbarComponent {
+
+  links:Nav[] = [];
+  siteName:string = "";
+
+  constructor(private navbarService: NavbarService){
+
+    navbarService.obtenerDatos().subscribe(respuesta => {
+            
+      let nav = respuesta as Navbar;
+
+      this.links = nav.links;
+      this.siteName = nav.siteName;
+    })
+
+  }
 
 }
---
+++ spa/clienteAngular/src/app/interfaz/navbar.ts
@@ -0,0 +1,11 @@
+export interface Navbar {
+
+    links:Nav[];
+    siteName:string;
+}
+
+export interface Nav{
+
+    href:string; 
+    text:string;
+}
