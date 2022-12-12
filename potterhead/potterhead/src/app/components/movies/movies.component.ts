452e22aa0725ef7b91e3998acba22da4a6932536
Cliente CONTROL-PC <gabamald@espol.edu.ec>
2022-12-12 10:06:03 -0500

    potterhead

---
+++ potterhead/potterhead/src/app/components/movies/movies.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { Movie, Attributes } from '../../interface/movie';
+
+@Component({
+  selector: 'app-movies',
+  templateUrl: './movies.component.html',
+  styleUrls: ['./movies.component.css']
+})
+export class MoviesComponent {
+
+  movies!: Movie[];
+
+  constructor() {
+
+    /* Leer desde el localStorage */
+    let potterhead = JSON.parse(localStorage.getItem("potterhead")!);
+      
+    if(potterhead) {
+      this.movies = potterhead as Movie[]
+    }
+    
+
+  }
+
+}
